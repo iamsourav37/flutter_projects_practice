b67c16718a820a4e0af74b7eb14e61d85622dd23
Sourav Ganguly <souravganguly108@gmail.com>
2021-01-07 13:35:12 +0530

    gridview builder example

--- practice_0/lib/main.dart
+++ practice_0/lib/main.dart
@@ -96,150 +96,39 @@ class Home extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.alarm),
       ),
-      body: GridView.count(
-        padding: EdgeInsets.all(2.0),
-        crossAxisCount: 2,
-        childAspectRatio: 1,
-        physics: BouncingScrollPhysics(),
-        mainAxisSpacing: 6.0,
-        crossAxisSpacing: 8.0,
-        children: [
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1609953578987-01712f9c4640?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          physics: BouncingScrollPhysics(),
+          itemCount: 100,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
           ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1529429612779-c8e40ef2f36d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+                print("You pressed : $index");
+              },
+              child: Material(
+                elevation: 1.3,
+                color: Colors.tealAccent,
+                shadowColor: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                textStyle: TextStyle(
+                  fontSize: 33.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                child: Center(
+                  child: Text(
+                    index.toString(),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1528901166007-3784c7dd3653?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1532522750741-628fde798c73?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1531498860502-7c67cf02f657?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1518818608552-195ed130cdf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1482062364825-616fd23b8fc1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTJ8fHByb2dyYW1tZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1563796021847-59711ad1dbd5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            height: 700,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/flagged/photo-1588272719848-c7eebb26de0a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ],
+            );
+          },
+        ),
       ),
     );
   }
