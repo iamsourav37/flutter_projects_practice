e4c4d730bf1836a26bfc1821929b97b72f84d26c
Sourav Ganguly <souravganguly108@gmail.com>
2021-01-08 19:15:14 +0530

    travel ui making

---
+++ travel_ui/lib/screens/home_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+  int _selectedItem = 0;
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedItem = index;
+        });
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: _selectedItem == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color: _selectedItem == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 100),
+              child: Text(
+                "What you would like to find?",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              color: Colors.yellow,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildIcon(0),
+                  _buildIcon(1),
+                  _buildIcon(2),
+                  _buildIcon(3),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
